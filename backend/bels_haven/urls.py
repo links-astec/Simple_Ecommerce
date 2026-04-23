cbba5660824e55805f779c6b947c2a7300825d4e
links-astec <gabbyquaye2021@gmail.com>
2026-04-23 04:08:37 +0200

    Fix media file serving in production and update render.yaml

--- backend/bels_haven/urls.py
+++ backend/bels_haven/urls.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('store.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Serve media files in production (Render)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
 
 admin.site.site_header = "Bel's Haven Admin"
 admin.site.site_title = "Bel's Haven"
